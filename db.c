b140de9b6d20120c03c1e75177e448efd695de43
Trần Thanh <tranthanh.it.95@gmail.com>
2024-09-20 19:44:56 +0700

    chore: simplest sql compiler (#2)

--- db.c
+++ db.c
@@ -10,6 +10,27 @@ struct Connection_t {
 
 typedef struct Connection_t Connection;
 
+struct InputBuffer_t {
+    char *buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+};
+
+typedef struct InputBuffer_t InputBuffer;
+
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum { PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT } PrepareResult;
+
+typedef enum { STATEMENT_INSERT, STATEMENT_SELECT } StatementType;
+
+typedef struct {
+    StatementType type;
+} Statement;
+
 Connection *
 open_connection(char *filename) {
     int fd = open(filename,
@@ -38,14 +59,6 @@ get_db_filename(int argc, char *argv[]) {
 
     return argv[1];
 }
-
-struct InputBuffer_t {
-    char *buffer;
-    size_t buffer_length;
-    ssize_t input_length;
-};
-
-typedef struct InputBuffer_t InputBuffer;
 
 InputBuffer *
 new_input_buffer() {
@@ -83,6 +96,41 @@ close_input_buffer(InputBuffer *input_buffer) {
     free(input_buffer);
 }
 
+MetaCommandResult
+do_meta_command(InputBuffer *input_buffer) {
+    if (strcmp(input_buffer->buffer, ".exit") == 0) {
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
+PrepareResult
+prepare_statement(InputBuffer *input_buffer, Statement *statement) {
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (strcmp(input_buffer->buffer, "select") == 0) {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void
+execute_statement(Statement *statement) {
+    switch (statement->type) {
+    case (STATEMENT_INSERT):
+        printf("This is where we would do an insert.\n");
+        break;
+    case (STATEMENT_SELECT):
+        printf("This is where we would do an select.\n");
+        break;
+    }
+}
+
 int
 main(int argc, char *argv[]) {
     char *db_filename = get_db_filename(argc, argv);
@@ -93,11 +141,27 @@ main(int argc, char *argv[]) {
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0) {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        } else {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        if (input_buffer->buffer[0] == '.') {
+            switch (do_meta_command(input_buffer)) {
+            case (META_COMMAND_SUCCESS):
+                continue;
+            case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                continue;
+            }
         }
+
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement)) {
+        case (PREPARE_SUCCESS):
+            break;
+        case (PREPARE_UNRECOGNIZED_STATEMENT):
+            printf("Unrecognized keyword at start of '%s'.\n",
+                   input_buffer->buffer);
+            continue;
+        }
+
+        execute_statement(&statement);
+        printf("Executed.\n");
     }
 }
